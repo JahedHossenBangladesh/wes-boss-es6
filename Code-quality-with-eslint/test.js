0648b1020d87fccdc697dea22da28e396f07588a
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-12-04 21:39:38 +0600

    eslint setup

---
+++ Code-quality-with-eslint/test.js
@@ -0,0 +1,41 @@
+const wes = Symbol("Wes");
+const person = Symbol("Wes");
+console.log(wes,person)
+const classRoom = {
+  [Symbol("Mark")]: { grade: 50, gender: "male" },
+  [Symbol("olivia")]: { grade: 80, gender: "female" },
+  [Symbol("olivia")]: { grade: 80, gender: "female" },
+};
+
+for (const person in classRoom) {
+  console.log(person);
+}
+
+const syms = Object.getOwnPropertySymbols(classRoom);
+const data = syms.map((sym) => classRoom[sym]);
+console.log(data);
+
+
+
+let people = {
+  name: "John",
+  age: 30,
+  city: "New York",
+  married: true,
+  kids: ["John", "Jane", "Mark"],
+};
+
+for (const key in people) {
+  const value = people[key];
+  console.log(`${key}: ${value}`);
+}
+const chlidList = people.kids.join(", ");
+console.log(chlidList);
+
+// for of loop with index
+for (const [index, value] of people.kids.entries()) {
+  if (value === "Jane") {
+    break;
+  }
+  console.log(value,index);
+}
