cfac78e6186b1c3a958fcba55c0f248ef28ceaa1
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-12-20 00:37:18 +0600

    new npm update

--- JavaScript Modules and Using npm/es6Modules/webpack.config.js
+++ JavaScript Modules and Using npm/es6Modules/webpack.config.js
@@ -1,39 +1,47 @@
-const webpack = require('webpack');
-const nodeEnv = process.env.NODE_ENV || 'development';
+// const webpack = require('webpack');
+// const nodeEnv = process.env.NODE_ENV || 'development';
+// module.exports = {
+// devtool: 'source-map',
+// entry: {
+//     filename:'./app.js'
+// },
+// output :{
+//     filename:'_build/bundle.js'
+// },
+// module:{
+//     loaders:[
+//         {
+//             test:/\.js$/,
+//             exclude:/node_modules/,
+//             loader:'babel',
+//             query:{
+//                 presets:['es2015-native-modules']
+//             }
+//         }
+//     ]
+// },
+// Plugin:[
+//     new webpack.optimize.UglifyJsPlugin({
+//         compress:{
+//             warnings:false
+//         },
+//         output:{
+//             comments:false
+//         },
+//         sourceMap:true
+//     }),
+//     new webpack.DefinePlugin({
+//         'process.env':{
+//             'NODE_ENV':JSON.stringify(nodeEnv)
+//         }
+//     })
+// ]
+// }
 module.exports = {
-devtool: 'source-map',
-entry: {
-    filename:'./app.js'
-},
-output :{
-    filename:'_build/bundle.js'
-},
-module:{
-    loaders:[
-        {
-            test:/\.js$/,
-            exclude:/node_modules/,
-            loader:'babel',
-            query:{
-                presets:['es2015-native-modules']
-            }
-        }
-    ]
-},
-Plugin:[
-    new webpack.optimize.UglifyJsPlugin({
-        compress:{
-            warnings:false
-        },
-        output:{
-            comments:false
-        },
-        sourceMap:true
-    }),
-    new webpack.DefinePlugin({
-        'process.env':{
-            'NODE_ENV':JSON.stringify(nodeEnv)
-        }
-    })
-]
-}
+  mode: process.env.NODE_ENV || "production",
+  devtool: "source-map",
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js",
+  },
+};
