3ae0554a2660cf7f860120ec90e2d3f8a24ce1f7
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-12-21 23:00:37 +0600

    The src

--- JavaScript Modules and Using npm/es6Modules/app.js
+++ JavaScript Modules and Using npm/es6Modules/app.js
@@ -1,2 +1,9 @@
-import { uniq } from "lodash";
-import insane from "insane";
+import insane from 'insane';
+import jsonp from 'jsonp';
+import { apiKey as key, url, sayHi, old, dog } from './src/config';
+
+import User, { createURL, gravatar } from './src/user';
+
+const wes = new User('Wes Bos', 'wesbos@gmail.com', 'wesbos.com');
+const profile = createURL(wes.name);
+const image = gravatar(wes.email);
