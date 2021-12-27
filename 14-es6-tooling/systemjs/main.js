412d5daa0ffaef6243f5470c1cb6b690f4a1655f
Jahed hossen <67328056+JahedHossenBangladesh@users.noreply.github.com>
2021-12-27 23:50:11 +0600

    fixed the problem

--- 14-es6-tooling/systemjs/main.js
+++ 14-es6-tooling/systemjs/main.js
@@ -3,4 +3,5 @@ import {addTax} from './checklist'
 
 // console.log(kebabCase('Jahed is cool'));
 console.log(addTax(100,0.1));
-// this is from git remote repo
+// this is from git remote repo\
+// this is from git remote repo\ and trying t o fixed the problem
